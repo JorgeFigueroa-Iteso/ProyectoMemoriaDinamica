78b5b02c2f0a023d22cf7104c37bc5f75cafb584
Jorge Ramón Figueroa Maya <jorge.figueroa@iteso.mx>
2022-09-15 08:43:49 -0500

    a

--- main.c
+++ main.c
@@ -3,8 +3,13 @@
 
 int main(void){
 
+	printf("Diferencia entre una mamada y otra es que uno dice:\n");
 
 	printf("Bienvenido sea al proyecto");
+
+	printf("Y otro dice:\n");
+
+	printf("Awa Awa\n");
 
 	// system("pause");
 	return 0;
