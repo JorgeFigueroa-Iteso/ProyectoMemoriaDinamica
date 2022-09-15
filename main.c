10d4d578b71a938ddb29adaaf07dab25f25ade93
Jorge Ramón Figueroa Maya <jorge.figueroa@iteso.mx>
2022-09-14 22:57:01 -0500

    a

--- main.c
+++ main.c
@@ -3,9 +3,12 @@
 
 int main(void){
 
-	for (int i = 0; i < 5; ++i)	{
+	for (int i = 0; i < 5; ++i)
+	{
 		printf("Awa Awa: %d\n", i+1);
 	}
+
+	printf("Hello World!");
 
 	// system("pause");
 	return 0;
