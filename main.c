410d50a14eef2d2f747d830034baee55764bce3d
Jorge Ramón Figueroa Maya <jorge.figueroa@iteso.mx>
2022-09-15 11:40:48 -0500

    a

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+// Comentarios*
 // Debe mostrar un menu principal, el cual muestre lo siguiente:
 // Dimensión del tablero
 // Selección de dificultad: - Didáctico - Fácil
 // Iniciar juego
 // Salir
 
+
 // --------------------------------------------------------------- //
 
 // Declaración de Tipos de Datos
 
+
+
 // --------------------------------------------------------------- //
 
 // Declaración de Estructuras
+
+
 
 // --------------------------------------------------------------- //
 
@@ -28,6 +35,20 @@ int main(void){
 	return 0;
 }
 
+// --------------------------------------------------------------- //
+
+// Espacio para utilizar las estructuras y tipos de datos
+
+
+
+// --------------------------------------------------------------- //
+
+
+
+
+
+// Publicar cambios a GitHub:
+
 // Pasos para actualizar el proyecto de Git desde Sublime Text:
 // Primero es el Add All, después el Commit y por último el Push
 
@@ -36,4 +57,11 @@ int main(void){
 // Entonces, para publicarlo, solo tendrás que picarle en Commit, posteriórmente abrirá una ventana se llama COMMIT_EDITMSG
 // Solo escribe a, guarda el archivo "ctrl + s", cierra la ventana y darle en Sync Changes, para que se publiquen los datos a Git
 
-// 
+
+// Jalar los cambios de Git a pc:
+
+// Pasos para actualizar el proyecto de pc desde Sublime Text:
+
+
+// Pasos para actualizar el proyecto de pc desde VisualStudio:
+
