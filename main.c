4e1950b15a6efb95f23d3d12bc9f10e87fdd65be
Jorge Ramón Figueroa Maya <jorge.figueroa@iteso.mx>
2022-09-18 21:56:51 -0500

    a

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Comentarios*
+// Comentarios
 // Debe mostrar un menu principal, el cual muestre lo siguiente:
 // Dimension del tablero
 // Seleccion de dificultad: - Didáctico - Fácil
