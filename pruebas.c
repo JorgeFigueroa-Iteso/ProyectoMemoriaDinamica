08f2b608de92d1eee6aa973ddc3cb68817feb881
Jorge Ramón Figueroa Maya <jorge.figueroa@iteso.mx>
2022-09-21 12:47:33 -0500

    Matriz medio funcional en prueba

--- pruebas.c
+++ pruebas.c
@@ -1,27 +1,92 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define SIZE 100
 
-typedef struct Color{
-	int r,b,g;
-}Color;
+void ins (char matrix1[SIZE][SIZE], char matrix2[SIZE][SIZE], int row, int column);
+void print (char matrix1[SIZE][SIZE], char matrix2[SIZE][SIZE], int row, int column);
+void celda(char matrix1[SIZE][SIZE]);
 
-int printImage(Color img[32][32],int, int);
+int main (){
 
-int main(void){
+	setbuf(stdin, NULL);
+	setbuf(stdout, NULL);
 
+    char userMatrix[SIZE][SIZE];
+    char botMatrix[SIZE][SIZE];
+    int row, col, x, y;
 
+    printf("Input rows: ");
+    scanf  ("%d", &row);
+    printf("Input columns: ");
+    scanf  ("%d", &col);
 
-	// system("pause");
-	return 0;
+    printf ("Input data: \n");
+    ins(userMatrix, botMatrix, row, col);
+
+    printf ("\nMatrices--\nMatriz 1:\n");
+    print(userMatrix, botMatrix, row, col);
+
+    // printf("Celda a modificar: ");
+	celda(userMatrix);
+    printf("Celda modificada:\n");
+    print(userMatrix, botMatrix, row, col);
+
+    return 0;
 }
 
-int printImage(Color img[32][32],int x, int y){
+void ins (char matrix1[SIZE][SIZE], char matrix2[SIZE][SIZE], int row, int column){
+    int i, j;
 
-	for (int i = 0; i < 32; ++i)
-	{
-		for (int j = 0; j < 32; ++j)
-		{
-			*(*(img+i)+j) = 0;
-		}
-	}
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+        	*(*(matrix1+i)+j) == 0;
+        }
+    }
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+        	*(*(matrix2+i)+j) == 0;
+        }
+    }
+}
+
+void print (char matrix1[SIZE][SIZE], char matrix2[SIZE][SIZE], int row, int column){
+    int i;
+    int j;
+
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<column; j++)
+        {
+            // printf("%d ", matrix[i][j]);
+            printf("%d ", *(*(matrix1+i)+j));
+        }
+        printf("\n");
+    }
+
+    printf("Matriz 2:\n");
+
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<column; j++)
+        {
+            // printf("%d ", matrix[i][j]);
+            printf("%d ", *(*(matrix2+i)+j));
+        }
+        printf("\n");
+    }
+}
+
+void celda (char matrix1[SIZE][SIZE]){
+	int x, y, nc;
+	printf("Coordenadas de la celda a modificar: \nx: ");
+	scanf("%d", &x);
+	printf("y: ");
+	scanf("%d", &y);
+	printf("Numero a asignar: ");
+	scanf("%d", &nc);
+	*(*(matrix1+x)+y) = nc-1;
 }
