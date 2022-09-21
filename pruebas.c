3d652d216e05c23c635ed5e7f8793eb3b9d391dc
Jorge Ramón Figueroa Maya <jorge.figueroa@iteso.mx>
2022-09-21 14:37:25 -0500

    Matrices, Dificultades y dimensiones funcionales

--- pruebas.c
+++ pruebas.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #define SIZE 100
 
-void ins (char matrix[SIZE][SIZE], int row, int column);
-void print (char matrix[SIZE][SIZE], int row, int column);
-void celda(char matrix[SIZE][SIZE]);
+void ins (char matrix[SIZE][SIZE], char matrixBot[SIZE][SIZE], int row, int column);
+void print (char matrix[SIZE][SIZE], char matrixBot[SIZE][SIZE], int row, int column);
+void celda(char matrix[SIZE][SIZE], char matrixBot[SIZE][SIZE]);
+
+void mostrarMenu();
 
 int main (){
 
@@ -11,29 +15,58 @@ int main (){
 	setbuf(stdout, NULL);
 
     char userMatrix[SIZE][SIZE];
-    char botMAtrix[SIZE][SIZE];
-    int row, col, x, y;
+    char botMatrix[SIZE][SIZE];
+    char op;
 
-    printf("Input rows: ");
-    scanf  ("%d", &row);
-    printf("Input columns: ");
-    scanf  ("%d", &col);
+    int row=15, col=15, x, y;
+    int dif=1;
 
     printf ("Input data: \n");
-    ins(userMatrix, row, col);
+    ins(userMatrix, botMatrix, row, col);
 
-    printf ("You entered: ");
-    print(userMatrix, row, col);
+    do{
+		system("cls");
+		mostrarMenu();
+		// printf("Seleccione una opcion (a-e): \n");
+		scanf("%c", &op);
+		switch(op){
+			case 'a':
+			    printf("Input rows: ");
+				scanf  ("%d", &row);
+				printf("Input columns: ");
+				scanf  ("%d", &col);
+				ins(userMatrix, botMatrix, row, col);
+			case 'b':
+				printf("Imprimir matriz a.\n");
+				print(userMatrix, botMatrix, row, col);
+				break;
+			case 'c':
+				printf("Seleccion de dificultad:\n(1) - Por Default\n");
+				printf("Dificultades disponibles:\n1.- Didactico\n2.- Facil\nnumero de la dificultad: ");
+				scanf("%d", &dif);
+				if (dif==1)
+				{
+					printf("Dificultad: Didactico\n");
+				} else{
+					printf("Dificultad: Facil\n");
+				}
+				system("timeout /t 3\n");
+				break;
+		}
+	}while(op!='e');
+
+/*    printf ("You entered: \n");
+    print(userMatrix, botMatrix, row, col);
 
     // printf("Celda a modificar: ");
 	celda(userMatrix);
     printf("Celda modificada:\n");
-    print(userMatrix, row, col);
-
+    print(userMatrix, botMatrix, row, col);
+*/
     return 0;
 }
 
-void ins (char matrix[SIZE][SIZE], int row, int column){
+void ins (char matrix[SIZE][SIZE], char matrixBot[SIZE][SIZE], int row, int column){
     int i, j;
 
     for (i = 0; i < row; i++)
@@ -43,12 +76,20 @@ void ins (char matrix[SIZE][SIZE], int row, int column){
         	*(*(matrix+i)+j) == 0;
         }
     }
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+        	*(*(matrixBot+i)+j) == 0;
+        }
+    }
 }
 
-void print (char matrix[SIZE][SIZE], int row, int column){
+void print (char matrix[SIZE][SIZE], char matrixBot[SIZE][SIZE], int row, int column){
     int i;
     int j;
-
+    printf("--Matriz 1--\n");
     for(i=0; i<row; i++)
     {
         for(j=0; j<column; j++)
@@ -58,15 +99,48 @@ void print (char matrix[SIZE][SIZE], int row, int column){
         }
         printf("\n");
     }
+    printf("--Matriz 2--\n");
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<column; j++)
+        {
+            // printf("%d ", matrix[i][j]);
+            printf("%d ", *(*(matrixBot+i)+j));
+        }
+        printf("\n");
+    }
+
+    system("timeout /t 3\n");
 }
 
-void celda (char matrix[SIZE][SIZE]){
+void celda (char matrix[SIZE][SIZE], char matrixBot[SIZE][SIZE]){
 	int x, y, nc;
-	printf("Coordenadas de la celda a modificar: \nx: ");
+	printf("Coordenadas de la celda a modificar - Matriz 1: \nx: ");
 	scanf("%d", &x);
 	printf("y: ");
 	scanf("%d", &y);
 	printf("Numero a asignar: ");
 	scanf("%d", &nc);
-	*(*(matrix+x)+y) = nc;
+
+	*(*(matrix+x-1)+y-1) = nc;
+
+	printf("Coordenadas de la celda a modificar - Matriz 2: \nx: ");
+	scanf("%d", &x);
+	printf("y: ");
+	scanf("%d", &y);
+	printf("Numero a asignar: ");
+	scanf("%d", &nc);
+
+	*(*(matrixBot+x-1)+y-1) = nc;
+
+	system("timeout /t 3\n");
+}
+
+void mostrarMenu(){
+	system("cls");
+	printf("a. Dimension del tablero\n");
+	printf("b. Ver tablero\n");
+	printf("c. Seleccion de dificultad\n");
+	printf("d. Iniciar Juego\n");
+	printf("e. Salir\n");
 }
